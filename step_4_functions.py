2d7fed9af8f117c7a6dc1652a36e25d2003afb29
Max Farnham <vsx8ws@virginia.edu>
2026-02-04 23:44:34 -0500

    Update step_4_functions.py

--- step_4_functions.py
+++ step_4_functions.py
@@ -3,8 +3,6 @@
 import pandas as pd  # For data manipulation and analysis
 import numpy as np  # For numerical operations
 import matplotlib.pyplot as plt  # For data visualization
-# Make sure to install sklearn in your terminal first!
-# Use: pip install scikit-learn
 from sklearn.model_selection import train_test_split  # For splitting data
 from sklearn.preprocessing import MinMaxScaler, StandardScaler  # For scaling
 from io import StringIO  # For reading string data as file
